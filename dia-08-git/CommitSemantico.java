bd1789085c3778f70fac05f4bbce7a6fb8dae6b4
Cristian Dornelles <c.dornelles97@gmail.com>
2026-07-08 18:47:24 -0300

    feat: adiciona metodo de saudacao

--- dia-08-git/CommitSemantico.java
+++ dia-08-git/CommitSemantico.java
@@ -2,6 +2,11 @@ public class CommitSemantico {
 
     public static void main(String[] args) {
         System.out.println("Praticando commits semanticos");
+        saudacao("Cristian");
+    }
+
+    public static void saudacao(String nome) {
+        System.out.println("Oi " + nome);
     }
 
 }
