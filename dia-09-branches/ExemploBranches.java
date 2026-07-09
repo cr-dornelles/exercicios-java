81fb3d31c51cc2fa9fa207397627b853ca638f07
Cristian Dornelles <c.dornelles97@gmail.com>
2026-07-09 12:10:18 -0300

    feat: adiciona metodo exibirInfo na feature branch

--- dia-09-branches/ExemploBranches.java
+++ dia-09-branches/ExemploBranches.java
@@ -2,6 +2,12 @@ public class ExemploBranches {
 
     public static void main(String[] args) {
         System.out.println("Criado na feature branch");
+        exibirInfo();
+    }
+
+    public static void exibirInfo() {
+        System.out.println("Branch: feature/dia-09-branches");
+        System.out.println("Operacao: merge com a main");
     }
 
 }
